2acaf5f7bf31069254023ec148d14a96d7edcc01
constself <constself@outlook.com>
2023-08-18 11:22:11 +0800

    feat: add

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 const BLOG = {
   title: "Pard",
   author: "constPc",
-  email: "i@craigary.net",
+  email: "pengchengG1@outlook.com",
   link: "https://nobelium.vercel.app",
   description: "This gonna be an awesome website.",
   lang: "zh-CN", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
@@ -18,7 +18,7 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: "https://og-image-craigary.vercel.app", // The link to generate OG image, don't end with a slash
-  socialLink: "https://twitter.com/craigaryhart",
+  socialLink: "https://twitter.com/ConstPc404",
   seo: {
     keywords: ["Blog", "Website", "Notion"],
     googleSiteVerification: "", // Remove the value or replace it with your own google site verification code
